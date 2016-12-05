8c7bd29cfeefeefb8fe39639de14965f8403eb56
Roma <marakromrom@gmail.com>
2016-12-05 20:25:44 +0300

    Restructured classes

--- Linar_Sharads/Main_Logic/Repository.cs
+++ Linar_Sharads/Main_Logic/Repository.cs
@@ -9,13 +9,13 @@ using Newtonsoft.Json;
 
 namespace Main_Logic
 {
-    class Repository
+    internal class Repository
     {
         public Request MakeQuery(string apiRequest)
         {
             using (var client = new HttpClient())
             {
-                string result = client.GetStringAsync(apiRequest).Result;
+                var result = client.GetStringAsync(apiRequest).Result;
                 return JsonConvert.DeserializeObject<Request>(result);
             }   
         }
@@ -28,17 +28,14 @@ namespace Main_Logic
         public void Split<T>(List<T> array, int size, ref List<List<T>> splitedLIst)
         {
             for (var i = 0; i < array.Count / size; i++)
-            {
                 splitedLIst.Add(array.Skip(i * size).Take(size).ToList());
-            }
         }
 
-        public List<double> GetKoefs(Request Query)
+        public List<double> GetKoefs(Request query)
         {
-            var approx = new Approximation();
             var listOfValues = new List<List<double>>();
             double t = 0;
-            foreach (var varpair in Query.dataset.data)
+            foreach (var varpair in query.dataset.data)
             {
                 listOfValues.Add(new List<double> { t, (double)varpair[1] });
                 t++;
@@ -49,12 +46,34 @@ namespace Main_Logic
             Split(listOfValues, (int)listOfValues.Count / 25, ref splittedList);
 
             var listOfKoef = new List<double>();
-            //foreach (var item in splittedList)
-            //{
-            //    listOfKoef.Add(approx.DataProceeding(item));
-            //}
-           splittedList.ForEach(n=> listOfKoef.Add(approx.DataProceeding(n)));
+
+           splittedList.ForEach(n=> listOfKoef.Add(DataProceeding(n)));
             return listOfKoef;
+        }
+
+        public double DataProceeding(List<List<double>> x)
+        {
+
+            double k, b;
+            GetApprox(x, out k, out b, x.Count);
+            return k;
+        }
+
+        public static void GetApprox(IReadOnlyList<List<double>> x, out double k, out double b, int n)
+        {
+            double sumx = 0;
+            double sumy = 0;
+            double sumx2 = 0;
+            double sumxy = 0;
+            for (var i = 0; i < n; i++)
+            {
+                sumx += x[i][0];
+                sumy += x[i][1];
+                sumx2 += x[i][0] * x[i][0];
+                sumxy += x[i][0] * x[i][1];
+            }
+            k = (n * sumxy - (sumx * sumy)) / (n * sumx2 - sumx * sumx);
+            b = (sumy - k * sumx) / n;
         }
     }
 }
